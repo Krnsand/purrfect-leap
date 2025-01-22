95b659792823c407774b32d8209c9facdc4d8fc1
borghanna <hanna.borg@medieinstitutet.se>
2025-01-22 13:26:50 +0100

    test

--- src/startmenu.ts
+++ src/startmenu.ts
@@ -1,39 +1,26 @@
-class StartMenu implements IScreen {
-    private startButton: Button;
-    private howToPlayButton: Button;
+class StartMenu implements Screen {
+  private startButton: Button;
 
-    constructor() {
-        this.startButton = new Button('START', '#F96B6B', 500, 250, 350, 100);
-        this.howToPlayButton = new Button('HOW TO PLAY', '#F0AB63', 500, 380, 350, 100);
-    }
+  constructor() {
+    this.startButton = new Button("START", "#F96B6B", 100, 50, 750, 280);
+  }
 
-    private drawButtons() {
-        this.startButton.draw();
-        this.howToPlayButton.draw();
-    }
+  private drawButtons() {
+    this.startButton.draw();
+  }
 
-    private drawTitle() {
-        push();
-        fill('#8B8985');
-        textFont('Fredoka', 80)
-        textStyle(BOLD);
-        textAlign('center', 'center');
-        text('PURRFECT LEAP', 705, 115);
+  private drawTitle() {
+    fill("#000");
+    stroke("#FFF");
+    textFont("Arial", 30); //test, should be Fredroka One
+    textAlign("center");
+    text("PURRFECT LEAP", 750, 150);
+  }
 
-        fill('#F96B6B');
-        text('PURRFECT LEAP', 700, 110);
-        pop();
-    }
+  public update() {}
 
-    public update() {}
-
-    public draw() {
-        push();
-        fill('#C2E1B5')
-        noStroke();
-        rect(390, 190, 580, 350, 50);
-        pop();
-        this.drawTitle();
-        this.drawButtons();
-    }
+  public draw() {
+    this.drawTitle();
+    this.drawButtons();
+  }
 }
